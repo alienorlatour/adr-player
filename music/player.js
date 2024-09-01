b2f2c0db4e04cf6dc59b7c010f17877171f90820
Aliénor Latour <alienor.latour@gmail.com>
2024-09-01 22:10:44 +0200

    Bigger player

--- music/player.js
+++ music/player.js
@@ -2,45 +2,21 @@ const playlist = {
     "songs": [
         {
             "track": "music/01rouge/frank1.mp3",
-            "title": "Brahms - Un titre très long avec les noms des mouvements et le numéro 259",
-            "colour": "#EF1925",
-            "cover": "logo-rouge.png"
+            "title": "Brahms - Sonate n° 3 op 5 en fa mineur",
+            // "colour": "#EF1925",
+            // "cover": "logo-rouge.png"
         },
         {
             "track": "music/01rouge/frank2.mp3",
-            "title": "Brahms - Un autre titre long avec les noms et le numéro de l'opus 658",
-            "colour": "#EF1925",
-            "cover": "music/01rouge/cover.png"
+            "title": "Brahms - Allegro maestoso, Andante, Scherzo, Intermezzo, Finale.",
+            // "colour": "#EF1925",
+            // "cover": "music/01rouge/cover.png"
         },
         {
             "track": "music/01rouge/frank3.mp3",
-            "title": "Brahms autre - Titre un peu long long avec le numéro de l'opus 95",
-            "colour": "#E6256C",
-            "cover": "music/01rouge/cover.png"
-        },
-        {
-            "track": "music/02jaune/lento.mp3",
-            "title": "Liszt - La couleur jaune est souvent associée à la lumière et à la joie.",
-            "colour": "#FFEC01",
-            "cover": "music/02jaune/cover.png"
-        },
-        {
-            "track": "music/03violet/other1.mp3",
-            "title": "Chopin - Le violet est une couleur associée à la créativité.",
-            "colour": "#9936DF",
-            "cover": "music/03violet/cover.png"
-        },
-        {
-            "track": "music/03violet/other2.mp3",
-            "title": "Chopin - Petit titre.",
-            "colour": "#9936DF",
-            "cover": "music/03violet/cover.png"
-        },
-        {
-            "track": "music/04vert/lento.mp3",
-            "title": "Beethoven - Le vert symbolise la nature, la croissance et la tranquillité.",
-            "colour": "#4BB167",
-            "cover": "music/04vert/cover.png"
+            "title": "Brahms - Von Ewiger Liebe, opus 43 n°1",
+            // "colour": "#EF1925",
+            // "cover": "music/01rouge/cover.png"
         }
     ]
 }
@@ -69,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
         changeAudioSource(song.track)
         currentlyPlaying.innerHTML = song.title
-        listenContainer.style.background = "linear-gradient(60deg, " + song.colour + " 75%, #000)"
+        // listenContainer.style.background = "linear-gradient(60deg, " + song.colour + " 75%, #000)"
 
     }
 
